769fbe3684884795056b1b037b0b4dbc04fff8c9
hwang <nmc2711@naver.com>
2021-11-15 13:28:24 +0900

    feat: login api test http & common hooks

---
+++ pages/api/login.ts
@@ -0,0 +1,25 @@
+import { fetchJson } from "../../lib/api";
+
+async function handler(req: any, res: any) {
+  if(req.method !== 'POST') {
+    res.status(405).end();
+    return;
+  }
+  const { email, password } = req.body;
+  try {
+    const { jwt, user } = await fetchJson('http://localhost:1337/auth/local', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ identifier: email, password }),
+      });
+      res.status(200).json({
+        id: user.id,
+        name: user.username,
+    });      
+  } catch(err) {
+    res.status(401).end();
+  }
+
+}
+
+export default handler;
