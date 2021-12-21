5c6594e2a6e412ee8ddbeb1372192bf1da811334
hwang <nmc2711@naver.com>
2021-12-21 10:00:12 +0900

    feat: 프로젝트 가이드 진행중

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 /**
- * @title : Page Root _app - View Controller
+ * @title : 모든 컴포넌트의 Root INDEX - _app.tsx
+ * @purpose : Redux, Query, Style 등 공통으로 사용될 Provider 또는 전역 모듈을 설정할 수 있다.
  */
-
 import type { AppProps } from "next/app";
 
 import { Provider } from 'react-redux';
@@ -19,16 +19,17 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        {/* 브라우저 탭 */}
+
+        {/* Browser Tab 접근성 */}
         <Head>
           <link rel="icon" href="/icons/favicon.ico" />
         </Head>
 
-        {/* 뷰 */}
+        {/* Child Components */}
         <Component {...pageProps} />
+
       </QueryClientProvider>
     </Provider>
   );
 }
-
 export default MyApp;
