46f81666c4e071c61c58b90c25786c4013f6b008
hwangsanghan <nmc2711@naver.com>
2021-12-29 18:47:50 +0900

    fix: lint space remove

--- toolkit/store.ts
+++ toolkit/store.ts
@@ -1,44 +1,44 @@
 import {
-    Action,
-    AnyAction,
-    combineReducers,
-    configureStore,
-    ThunkAction,
-  } from '@reduxjs/toolkit';
-  import { createWrapper, HYDRATE } from 'next-redux-wrapper';
-  import { counterReducer } from '../features/counter';
+  Action,
+  AnyAction,
+  combineReducers,
+  configureStore,
+  ThunkAction,
+} from '@reduxjs/toolkit';
+import { createWrapper, HYDRATE } from 'next-redux-wrapper';
+import { counterReducer } from '../features/counter';
   
-  const combinedReducer = combineReducers({
-    counter: counterReducer,
+const combinedReducer = combineReducers({
+  counter: counterReducer,
+});
+  
+const reducer = (state: any, action: AnyAction) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state,
+      ...action.payload,
+    };
+    return nextState;
+  } else {
+    return combinedReducer(state, action);
+  }
+};
+  
+export const makeStore = () =>
+  configureStore({
+    reducer,
   });
   
-  const reducer = (state: any, action: AnyAction) => {
-    if (action.type === HYDRATE) {
-      const nextState = {
-        ...state,
-        ...action.payload,
-      };
-      return nextState;
-    } else {
-      return combinedReducer(state, action);
-    }
-  };
+type Store = ReturnType<typeof makeStore>;
   
-  export const makeStore = () =>
-    configureStore({
-      reducer,
-    });
+export type AppDispatch = Store['dispatch'];
+export type RootState = ReturnType<Store['getState']>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
   
-  type Store = ReturnType<typeof makeStore>;
-  
-  export type AppDispatch = Store['dispatch'];
-  export type RootState = ReturnType<Store['getState']>;
-  export type AppThunk<ReturnType = void> = ThunkAction<
-    ReturnType,
-    RootState,
-    unknown,
-    Action<string>
-  >;
-  
-  export const wrapper = createWrapper(makeStore, { debug: true });
+export const wrapper = createWrapper(makeStore, { debug: true });
   
