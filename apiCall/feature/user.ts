924143448899f7948975fc57eb8b38c14f421d74
hwangsanghan <nmc2711@naver.com>
2022-01-13 11:53:36 +0900

    feat: fetch 불필요한 로직제거

--- apiCall/feature/user.ts
+++ apiCall/feature/user.ts
@@ -1,10 +1,6 @@
-import { AxiosResponse } from 'axios';
-import { useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 
-import { fetchJson } from '../../lib/api'
-import type { User } from '../../@types';
-
-import { axiosInstance } from '../axios';
+import { fetchJson } from '../../lib/api';
 
 const USER_QUERY_KEY = 'user';
 
@@ -21,17 +17,3 @@ export function useUser() {
   });
   return query.data;
 }
-
-// export function useUser() {
-//     const query = useQuery(USER_QUERY_KEY, async () => {
-//       try {
-//         return await fetchJson('/api/user');
-//       } catch (error) {
-//         return undefined;
-//       }
-//         }, {
-//         cacheTime: Infinity,
-//         staleTime: 30_000,
-//       });
-//     return query.data;
-//   }
