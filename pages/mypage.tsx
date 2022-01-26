77f3e7e94206e16af4164556ecea173f18437ce6
hwangsanghan <nmc2711@naver.com>
2022-01-26 09:55:57 +0900

    feat: next-image loader

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -6,6 +6,10 @@ import Page from 'components/common/Page';
 
 import Head from "next/head";
 
+import Image from 'next/image';
+
+import CrossThumbImg from 'asset/background/cross_thumb.png';
+
 const MyPage: NextPage = () => {
   return (
     <Page title="헛둘 마이페이지">
@@ -14,7 +18,10 @@ const MyPage: NextPage = () => {
         <meta name="description" content="푸렌드 마이페이지" />
       </Head>
 
-      <h1>마이페이지</h1>
+      <div>
+        Static 이미지 테스트
+        <Image src={CrossThumbImg} width={500} height={500} />
+      </div>
 
     </Page>
   );
