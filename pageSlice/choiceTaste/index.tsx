379f3bf826f574e6b7a32c4bf11f4e789c61c93f
hwangsanghan <nmc2711@naver.com>
2022-02-07 09:43:24 +0900

    feat:취향선택 컨텐츠 초안 진행중

---
+++ pageSlice/choiceTaste/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChoiceTasteComponent() {
+  return (
+    <div>
+            
+    </div>
+  )
+}
+
+export default ChoiceTasteComponent
---
+++ pageSlice/choiceTaste/steps/gender.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function GenderStepComponent() {
+  return (
+    <div>
+            
+    </div>
+  )
+}
+export default GenderStepComponent
