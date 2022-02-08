7d9b707079d59bca6ecdf24c2d0f90247a0c590b
hwangsanghan <nmc2711@naver.com>
2022-02-08 11:37:50 +0900

    fix: 다크모드 컬러값 임시제거

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,5 +1,5 @@
 import { extendTheme, ThemeConfig } from '@chakra-ui/react';
-import { createBreakpoints, mode } from '@chakra-ui/theme-tools';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
@@ -28,37 +28,32 @@ const theme = extendTheme({
     s: '14px',
     xs: '12px',
   },
-  colors: (props: any) => ({
+  colors: {
     dGray: {
-      900: mode('red', 'blue')(props),
+      900: '#222222',
+      800: 'rgba(34,34,34,0.75)',
+      700: 'rgba(34,34,34,0.6)',
+      600: 'rgba(34,34,34,0.4)',
+      500: 'rgba(34,34,34,0.25)',
+      400: 'rgba(34,34,34,0.15)',
+      300: 'rgba(34,34,34,0.08)',
+      200: 'rgba(34,34,34,0.03)',
+      100: 'rgba(34,34,34,0.02)',
+      0: 'rgba(34,34,34,0)',
     },
- }),
-  // colors: {
-  //   dGray: {
-  //     900: ['#222222', 'rgba(34,34,34,0)'],
-  //     800: ['rgba(34,34,34,0.75)', 'rgba(34,34,34,0.85)'],
-  //     700: ['rgba(34,34,34,0.6)', 'rgba(34,34,34,0.7)'],
-  //     600: ['rgba(34,34,34,0.4)', 'rgba(34,34,34,0.5)'],
-  //     500: ['rgba(34,34,34,0.25)', 'rgba(34,34,34,0.45)'],
-  //     400: ['rgba(34,34,34,0.15)', 'rgba(34,34,34,0.15)'],
-  //     300: ['rgba(34,34,34,0.08)', 'rgba(34,34,34,0.35)'],
-  //     200: ['rgba(34,34,34,0.03)', 'rgba(34,34,34,0.08)',],
-  //     100: ['rgba(34,34,34,0.02)', 'rgba(34,34,34,0.06)'],
-  //     0: ['rgba(34,34,34,0)', 'rgba(34,34,34,0.03)'],
-  //   },
-  //   dRed: {
-  //     500: ['#E12418', '#E12418'],
-  //   },
-  //   dBg: {
-  //     dark: ['#222222', '#222222'],
-  //     gradientRed: '#e83d3d',
-  //     gradientYellow: '#DBAA0C',
-  //     gradientBlue: '#09B5B5',
-  //     gradientPurple: '#8E5DDE',
-  //     lightWhite: '#ffffff',
-  //   },
-  //   dPrimary: ['#7BC600', '#7BC600', '#fff'],
-  // },
+    dRed: {
+      500: '#E12418',
+    },
+    dBg: {
+      dark: '#222222',
+      gradientRed: '#e83d3d',
+      gradientYellow: '#DBAA0C',
+      gradientBlue: '#09B5B5',
+      gradientPurple: '#8E5DDE',
+      lightWhite: '#ffffff',
+    },
+    dPrimary: '#7BC600',
+  },
 });
 
 export default theme;
