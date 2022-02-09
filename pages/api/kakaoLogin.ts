d9590e0fecb9b25f0e1f5ddfdb9ce8921287cc69
hwangsanghan <nmc2711@naver.com>
2022-02-09 18:19:41 +0900

    window check

--- pages/api/kakaoLogin.ts
+++ pages/api/kakaoLogin.ts
@@ -16,11 +16,19 @@ const handler = async (req: any, res: any) => {
       body: JSON.stringify({ accessToken }),
     });
 
+    if (typeof window !== "undefined" && window) {
+      window.sessionStorage.setItem('jwt', result.token);
+      window.sessionStorage.setItem('idAuth', result.user.id);
+    }
     
     res.status(200)
-    .setHeader('Set-Cookie', cookie.serialize('jwt', result.token ))
+    .setHeader('Set-Cookie', cookie.serialize('jwt', result.token, {
+      path: '/api',
+      httpOnly: true,
+    }))
     .setHeader('Set-Cookie', cookie.serialize('idAuth', result.user.id ))
     return res.json({
+      jwt: result.token,
       id: result.user.id,
       name: result.user.nickname,
     })
