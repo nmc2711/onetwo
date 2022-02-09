93b5d04261924056d3b0b791da41373529af616d
hwangsanghan <nmc2711@naver.com>
2022-02-09 16:56:02 +0900

    쿠키 expires 설정

--- pages/api/kakaoLogin.ts
+++ pages/api/kakaoLogin.ts
@@ -9,6 +9,9 @@ const handler = async (req: any, res: any) => {
   const { accessToken } = req.body;
   
   try {
+    const expires = new Date()
+    expires.setDate(Date.now() + 1);
+
     const { result } = await fetchJson(`http://ec2-54-180-30-10.ap-northeast-2.compute.amazonaws.com:5510/api/v1/users/kakao-login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json;charset=UTF-8' },
@@ -18,11 +21,13 @@ const handler = async (req: any, res: any) => {
     .setHeader('Set-Cookie', cookie.serialize('jwt', result.token, {
       path: '/api',
       httpOnly: true,
+      expires,
     }))
     .setHeader('Set-Cookie', cookie.serialize('idAuth', result.user.id))
     return res.json({
       id: result.user.id,
       name: result.user.nickname,
+      expires,
     })
   } catch (err) {
     res.status(401).end();
