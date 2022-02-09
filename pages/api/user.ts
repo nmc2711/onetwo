50b2362ccf9f93e381d88418701bdf99616c53fb
hwangsanghan <nmc2711@naver.com>
2022-02-09 18:48:36 +0900

    headers change

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -4,12 +4,12 @@ const handleUser = async(req:any, res:any) => {
     
     const { jwt, idAuth } = req.cookies;
     console.log(req.cookies)
-    // if (!idAuth) {
-    //   res.status(401).end();
-    //   return;
-    // }
+    if (!idAuth) {
+      res.status(401).end();
+      return;
+    }
     try {
-      const user = await fetchJson(`http://ec2-54-180-30-10.ap-northeast-2.compute.amazonaws.com:5510/api/v1/users/${idAuth}`, {
+      const user = await fetchJson(`http://ec2-54-180-30-10.ap-northeast-2.compute.amazonaws.com:5510/api/v1/users`, {
         headers: { 'Authorization': `Bearer ${jwt}` },
       });
       res.status(200).json({
