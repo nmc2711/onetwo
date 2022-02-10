69e6575a84cb78af14e4c1aa37edc7bf2db0a866
hwangsanghan <nmc2711@naver.com>
2022-02-10 11:26:15 +0900

    feat: 필독 컴포넌트 추가

--- components/common/AdBanner/review.tsx
+++ components/common/AdBanner/review.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box, Text } from "@chakra-ui/react";
+import Image from 'next/image';
+
+import ArrowWhiteImg from 'asset/union/arrow_white_s.png';
+import SmileIcon from 'asset/union/smile_icon.png';
+import ForkIcon from 'asset/union/fork_knife_s.png';
 
 function FirstReviewBanner() {
   return (
-    <Flex bg="#7CC800" height="96px" m="18px 20px 40px" p="19px 20px" borderRadius="12px" justify="center" align="center" >
-      <h4 style={{ color: '#fff', fontWeight: 800, fontSize: '21px' }}>첫 리뷰를 남겨보세요</h4>
+    <Flex bg="dPrimary" height="104px" m="18px 20px 40px" p="20px 24px" justify="center" borderRadius="12px"  flexDirection="column">
+      <Flex alignItems="center" h="28px">
+         <Text h="28px" backgroundColor="#fff" borderRadius="38px" p="2px 14px" fontSize="s" fontWeight="bold">필독</Text>
+         <Box w="20px" h="20px" ml="10px">
+          <Image src={SmileIcon} alt="스마일 이미지" />
+        </Box>
+        <Box w="13px" h="18px" ml="10px">
+          <Image src={ForkIcon} alt="포크 이미지" />
+        </Box>
+      </Flex>
+      <Flex alignItems="center" mt="4px">
+        <Text fontSize="xl" fontWeight="800" color="#fff">첫 리뷰를 남겨보세요</Text>
+
+        <Box ml="auto" w="16px" h="16px">
+          <Image src={ArrowWhiteImg} alt="화살표 이미지" />
+        </Box>
+      </Flex>
     </Flex>
   )
 }
