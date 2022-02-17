36c4319b3759e07af3a723ef9f79726dd7dc6490
hwangsanghan <nmc2711@naver.com>
2022-02-17 11:05:28 +0900

    무한스크롤 io checker 및 커스텀 함수 구현중

--- hooks/infiniteScroll/index.tsx
+++ hooks/infiniteScroll/index.tsx
@@ -1,5 +1,7 @@
 import { forwardRef, ReactNode, useEffect } from "react";
 
+import useObserveScroll from './useObserverScroll';
+
 interface TProps {
   callback?(): void;
   children: ReactNode;
@@ -11,6 +13,12 @@ interface TProps {
 
 const InfinityCompoent = forwardRef<HTMLDivElement, TProps>(
   ({ children, ...props }, ref: any) => {
+    const { callback, itemkey, leng, rootmargin, threshold } = props;
+    const checkLastRef = leng === itemkey + 1 ? ref : null;
+    const { entry } = useObserveScroll(checkLastRef, {
+      threshold,
+      rootMargin: rootmargin,
+    })
     return (
       <div ref={ref}>{children}</div>
     )
