92b4c0c6d7d810bebe968e54cc00f483c3146406
hwang <nmc2711@naver.com>
2022-02-20 09:34:17 +0900

    infinite scorll api연동

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,19 +31,18 @@ export const getServerSideProps: GetServerSideProps<ReviewProps> = async () => {
 }
 
 const HomePage: React.FC<ReviewProps> = ({ products }) => {
-   const { result } = products;
+  const { result } = products;
   const ref = useRef<HTMLDivElement | null>(null);
 
-  const fakeList = result.list;
-
   const [list, setList] = useState(result.list);
-  const [page, setPage] = useState(1);
+  const [rid, setRid] = useState(result.lastId);
   
   const { value } = useAppSelector((state) => state.choice);
   
-  const scrollCallback = () => {
-    setList(list.concat(fakeList));
-    setPage(page + 1);
+  const scrollCallback = async() => {
+    const scrolls = await getProducts({ tags:'NO_KIDS_ZONE', tag: 'CHEAP', page: 1, limit: 5, lastId: '' });
+    setList(list.concat(scrolls.result.list));
+    setRid(scrolls.result.lastId);
   }
 
   return (
