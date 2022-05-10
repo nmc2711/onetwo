88b4678059c7f8fb321d14bf7cebcb272993d75d
hwangsanghan <nmc2711@naver.com>
2022-05-10 10:17:13 +0900

    로그인 대기화면 진행중

--- components/Background/circleSum.tsx
+++ components/Background/circleSum.tsx
@@ -8,14 +8,29 @@ const CircleSum = () => {
   const { width }: any = getWindowSize();
 
   return (
-    <Flex p="20px" h="max-content" position="relative" minHeight="40vh" overflow="hidden" >
-      <Box pos="absolute" top="10%" w={`${+width * 0.2}px`} h={`${+width * 0.2}px`} borderRadius="50%" bg="blue" className="slide-right">
+    <Flex h="max-content" position="relative" minHeight="42vh" overflow="hidden" >
+      <Box pos="absolute" left="22.5%" top="12%" w={`${+width * 0.2}px`} h={`${+width * 0.2}px`} borderRadius="50%" bg="blue" className="slide-right">
         <div></div>
       </Box>
-      <Box pos="absolute" right="20%" w={`${+width * 0.3}px`} h={`${+width * 0.3}px`} borderRadius="50%" bg="red" className="slide-right">
+      <Box pos="absolute" right="7.2%" top="16%" w={`${+width * 0.245}px`} h={`${+width * 0.245}px`} borderRadius="50%" bg="red" className="slide-left">
         <div></div>
       </Box>
-      <Box position="absolute" right="50%" top="30%" w={`${+width * 0.2}px`} h={`${+width * 0.2}px`} borderRadius="50%" bg="green" className="slide-top">
+      <Box position="absolute" left="2%" top="36%" w={`${+width * 0.25}px`} h={`${+width * 0.25}px`} borderRadius="50%" bg="green" className="slide-right">
+        <div></div>
+      </Box>
+      <Box position="absolute" left="38%" top="32%" w={`${+width * 0.31}px`} h={`${+width * 0.31}px`} borderRadius="50%" bg="purple" className="slide-left">
+        <div></div>
+      </Box>
+      <Box position="absolute" right="-15%" top="38%" w={`${+width * 0.2}px`} h={`${+width * 0.2}px`} borderRadius="50%" bg="yellow" className="slide-right">
+        <div></div>
+      </Box>
+      <Box position="absolute" left="-15%" top="60%" w={`${+width * 0.2}px`} h={`${+width * 0.2}px`} borderRadius="50%" bg="gray" className="slide-left">
+        <div></div>
+      </Box>
+      <Box position="absolute" left="22%" top="70%" w={`${+width * 0.2}px`} h={`${+width * 0.2}px`} borderRadius="50%" bg="pink" className="slide-bottom">
+        <div></div>
+      </Box>
+      <Box position="absolute" right="4%" top="54%" w={`${+width * 0.28}px`} h={`${+width * 0.28}px`} borderRadius="50%" bg="skyblue" className="slide-right">
         <div></div>
       </Box>
     </Flex>
--- pages/login_choice.tsx
+++ pages/login_choice.tsx
@@ -16,45 +16,48 @@ const LoginChoice = () => {
   return (
     <Box h="100vh">
       <DefaultLogo />
-      <Flex p="26px 20px 20px 20px" flexDirection="column">
-        <Box mb="16px">
-          <Text 
-            h="28px" 
-            fontSize="xxxl" 
-            fontWeight="800" 
-            maxWidth="80%" 
-            lineHeight="40px" 
-            letterSpacing="-0.5px"
-          >
-            맛집을 공유하고
-          </Text>
+      <Flex p="26px 0 20px" flexDirection="column">
+        <Box p="0 20px">
+          <Box mb="16px">
+            <Text 
+              h="28px" 
+              fontSize="xxxl" 
+              fontWeight="800" 
+              maxWidth="80%" 
+              lineHeight="40px" 
+              letterSpacing="-0.5px"
+            >
+              맛집을 공유하고
+            </Text>
+          </Box>
+          <Box mb="16px">
+            <Text 
+              h="28px" 
+              fontSize="xxxl" 
+              fontWeight="800" 
+              maxWidth={+innerWidth > 359 ? "80%" : "100%"} 
+              lineHeight="40px" 
+              letterSpacing="-0.5px"
+            >
+              취향이 맞는 사람과 함께 
+            </Text>
+          </Box>
+          <Box display="flex">
+            <Text 
+              h="28px" 
+              fontSize="xxxl" 
+              fontWeight="800" 
+              maxWidth="80%" 
+              lineHeight="40px" 
+              letterSpacing="-0.5px"
+              mr="6px"
+            >
+              시간을 보내세요
+            </Text>
+            <Image src={DoubleCloneSvg} />
+          </Box>
         </Box>
-        <Box mb="16px">
-          <Text 
-            h="28px" 
-            fontSize="xxxl" 
-            fontWeight="800" 
-            maxWidth={+innerWidth > 359 ? "80%" : "100%"} 
-            lineHeight="40px" 
-            letterSpacing="-0.5px"
-          >
-            취향이 맞는 사람과 함께 
-          </Text>
-        </Box>
-        <Box display="flex">
-          <Text 
-            h="28px" 
-            fontSize="xxxl" 
-            fontWeight="800" 
-            maxWidth="80%" 
-            lineHeight="40px" 
-            letterSpacing="-0.5px"
-            mr="6px"
-          >
-            시간을 보내세요
-          </Text>
-          <Image src={DoubleCloneSvg} />
-        </Box>
+
 
         <CircleSum />
       </Flex>
